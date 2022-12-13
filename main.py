e43585072fd51b4e319c5ad8647b64fcc954eb34
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-12-13 14:37:46 +0000

    update api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import azure.functions as func
 
+
 app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
@@ -7,3 +8,5 @@ app = func.FunctionApp()
 
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("HttpTrigger1 function processed a request!")
+
+test_function("https://eventhubazurehomeassistant.azurewebsites.net")
