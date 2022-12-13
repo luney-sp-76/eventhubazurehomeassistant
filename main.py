23ff7424b40231d74a315dff7f12423a88c20b1f
Paul Olphert <69307367+luney-sp-76@users.noreply.github.com>
2022-12-13 13:12:12 +0000

    added function

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ app = func.FunctionApp()
 
 @app.function_name(name="HttpTrigger1")
 @app.route(route="hello")
+
 def test_function(req: func.HttpRequest) -> func.HttpResponse:
     return func.HttpResponse("HttpTrigger1 function processed a request!")
