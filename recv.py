3638da347462dc9fc6e5a1d6f36035055840fdde
Paul Olphert <polphert03@qub.ac.uk>
2023-01-02 22:20:31 +0000

    redact changes

--- recv.py
+++ recv.py
@@ -28,24 +28,8 @@ async def main():
         # Call the receive method. Read from the beginning of the partition (starting_position: "-1")
         await client.receive(on_event=on_event,  starting_position="-1")
 
-from functools import wraps
 
 
-from asyncio.proactor_events import _ProactorBasePipeTransport
- 
-def silence_event_loop_closed(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        try:
-            return func(self, *args, **kwargs)
-        except RuntimeError as e:
-            if str(e) != 'Event loop is closed':
-                raise
-    return wrapper
- 
-_ProactorBasePipeTransport.__del__ = silence_event_loop_closed(_ProactorBasePipeTransport.__del__)
-asyncio.set_event_loop(asyncio.ProactorEventLoop())
-
-loop = asyncio.get_event_loop()
+loop = asyncio.get_running_loop()
 loop.run_until_complete(main())
 loop.close()
