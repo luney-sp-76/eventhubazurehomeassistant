fc0e6b1be92733e8a04e732a02d6e2c0dc99a3aa
Paul Olphert <polphert03@qub.ac.uk>
2023-01-02 22:36:40 +0000

    new recv.py

--- recv.py
+++ recv.py
@@ -1,5 +1,5 @@
 import asyncio
-from azure.eventhub.aio import EventHubConsumerClient as client
+from azure.eventhub.aio import EventHubConsumerClient
 from azure.eventhub.extensions.checkpointstoreblobaio import BlobCheckpointStore
 import json
 
@@ -10,26 +10,43 @@ json_str = json.dumps(data)
 
 auth = json.loads(json_str)
 
+CONNECTION_STR = auth["CONNECTION_STRING"]
+EVENTHUB_NAME = auth['EVENT_HUB_NAME']
+STORAGE_CONNECTION_STR = auth["AZURE_STORAGE_CONNECTION_STRING"]
+BLOB_CONTAINER_NAME = "homeassistant"  # Please make sure the blob container resource exists.
+
 
 async def on_event(partition_context, event):
-    # Print the event data.
-    print("Received the event: \"{}\" from the partition with ID: \"{}\"".format(event.body_as_str(encoding='UTF-8'), partition_context.partition_id))
-
-    # Update the checkpoint so that the program doesn't read the events
-    # that it has already read when you run it next time.
+    # Put your code here.
+    print("Received event from partition: {}.".format(partition_context.partition_id))
     await partition_context.update_checkpoint(event)
 
+
+async def receive(client):
+    """
+    Without specifying partition_id, the receive will try to receive events from all partitions and if provided with
+    a checkpoint store, the client will load-balance partition assignment with other EventHubConsumerClient instances
+    which also try to receive events from all partitions and use the same storage resource.
+    """
+    await client.receive(
+        on_event=on_event,
+        starting_position="-1",  # "-1" is from the beginning of the partition.
+    )
+    # With specified partition_id, load-balance will be disabled, for example:
+    # await client.receive(on_event=on_event, partition_id='0'))
+
+
 async def main():
-    # Create an Azure blob checkpoint store to store the checkpoints.
-    checkpoint_store = BlobCheckpointStore.from_connection_string(auth['AZURE_STORAGE_CONNECTION_STRING'], "homeassistant")
-
-    # Create a consumer client for the event hub.
+    checkpoint_store = BlobCheckpointStore.from_connection_string(STORAGE_CONNECTION_STR, BLOB_CONTAINER_NAME)
+    client = EventHubConsumerClient.from_connection_string(
+        CONNECTION_STR,
+        consumer_group="$Default",
+        eventhub_name=EVENTHUB_NAME,
+        checkpoint_store=checkpoint_store,  # For load-balancing and checkpoint. Leave None for no load-balancing.
+    )
     async with client:
-        # Call the receive method. Read from the beginning of the partition (starting_position: "-1")
-        await client.receive(on_event=on_event,  starting_position="-1")
+        await receive(client)
 
 
-
-loop = asyncio.get_running_loop()
-loop.run_until_complete(main())
-loop.close()
+if __name__ == '__main__':
+    asyncio.run(main())
